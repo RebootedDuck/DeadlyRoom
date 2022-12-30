a329404b12580fe1bb4f743aa3f1b2dcd1e5cbb3
Rebooted Duck <88654965+RebootedDuck@users.noreply.github.com>
2022-12-30 21:14:23 +0000

    Add error handling

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-import fractions
+from math import trunc
+from fractions import Fraction
 
 print("Deadly Room V2.0 - Inspired by https://qr.ae/prvX6x")
 print("---")
@@ -7,22 +7,36 @@ print("---")
 type = input("Litres or Dimensions? L/D ")
 
 if type == "D":
-    height = float(input("How high is the room in meters?: "))
-    length = float(input("How long is the room in meters?: "))
-    width = float(input("How wide is the room in meters?: "))
+    try:
+        height = float(input("How high is the room in meters?: "))
+        length = float(input("How long is the room in meters?: "))
+        width = float(input("How wide is the room in meters?: "))
+    except ValueError:
+        exit("Not a Float")
+
     volume = height * width * length
     litres = volume * 1000
+elif type == "L":
+    try:
+        litres = float(input("How many litres? "))
+    except ValueError:
+        exit("Not a Float")
 else:
-    litres = float(input("How many litres? "))
+    exit("Invalid Type - L/D")
 
-people = float(input("How many people in the room?: "))
-otwo = math.trunc(litres / 5)
+try:
+    people = float(input("How many people in the room?: "))
+except ValueError:
+    exit("Not a Float")
+
+otwo = trunc(litres / 5)
 days = otwo / 550 / people
 roundays = round(days, 2)
-actualdays = roundays / fractions.Fraction(3, 2)
+actualdays = roundays / Fraction(3, 2)
 actualroundays = round(actualdays, 2)
 
 if type == "D":
+    # noinspection PyUnboundLocalVariable
     print('Room volume is ' + str(volume) + " metres cubed or " + str(litres) + " litres")
 if type == "L":
     print('Room volume is ' + str(litres) + ' litres')
@@ -30,4 +44,3 @@ if type == "L":
 print('Available uncompressed oxygen is ' + str(otwo) + ' litres')
 print('At maximum lung efficiency ' + str(people) + ' people could survive for ' + str(roundays) + ' days')
 print('However due to oxygen deprivation the actual time is ' + str(actualroundays) + ' days')
-
